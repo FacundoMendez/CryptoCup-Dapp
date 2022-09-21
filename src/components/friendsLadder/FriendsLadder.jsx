b80e9216ef0241dd0944a15598dddd42ee9afbd7
facuMendez <facundo.mendez@live.com.ar>
2022-09-21 13:59:12 -0300

    agregando friendsGame

--- src/components/friendsLadder/FriendsLadder.jsx
+++ src/components/friendsLadder/FriendsLadder.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React , {useState} from 'react'
+import NavLadder from "../ladder/componentsLadders/navLadder/NavLadder"
+import MyPredictions from '../ladder/componentsLadders/myPrediction/MyPredictions'
+import Ranking from '../ladder/componentsLadders/ranking/Ranking'
+import Predictions from '../ladder/componentsLadders/predictions/Predictions'
+import "./friendsLadder.css"
+
 
 const FriendsLadder = () => {
+
+  const [predictions , setPredictions] = useState(true)
+  const [myPrediction , setMyPrediction] = useState(false)
+  const [ranking , setRanking] = useState(false)
+
+
   return (
-    <div>FriendsLadder</div>
+  <div className="friendsLadder"> 
+      <NavLadder 
+          ladder="friends Ladder"
+          setMyPrediction={setMyPrediction}
+          setPredictions={setPredictions}
+          setRanking={setRanking}
+      />
+      <div className="container_friendsLadder">
+          {
+            predictions ? 
+              <Predictions/>
+            :
+            null
+          }
+
+          {
+            myPrediction ? 
+              <MyPredictions/>
+            :
+            null
+          }
+          
+          {
+            ranking ? 
+              <Ranking/>
+            :
+            null
+          }
+        </div>
+    </div>
   )
 }
 
