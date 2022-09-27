42f043a3b7a4dda9429fe37f10b78e9bc641dc5b
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2022-09-27 02:34:12 -0300

    get user

--- src/components/nav/verifyUser.js
+++ src/components/nav/verifyUser.js
@@ -3,7 +3,6 @@ import api from "../../api"
 export const verificarExistencia = async (signer, signature)=>{
     try {
         const res = await api.post('/user', { address:signer, signature });
-        console.log(res)
 
         if (res.data==="User does not exist.") {
             return false
@@ -21,9 +20,7 @@ export const verificarExistencia = async (signer, signature)=>{
 
 export const getUserData = async (signer, signature)=>{
     try {
-        console.log(signer)
         const res = await api.post('/user/getUser', { address: signer, signature });
-        console.log(res)
         return res.data
     } catch (error) {
         console.log(error)
