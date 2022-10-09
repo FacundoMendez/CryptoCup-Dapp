c6822a2c301ac5ff8bf1c3f854a5dba9293b91bb
FacuMendez <facundo.mendez@live.com.ar>
2022-10-09 00:32:42 -0300

    filter responsive

--- src/components/matchFeed/filterMatch/FilterMatch.jsx
+++ src/components/matchFeed/filterMatch/FilterMatch.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import "./filterMatch.css"
 import filterMatchFucional from './filterMatchFucional'
+import filterImg from "../../config/src/filter.png"
 
 const FilterMatch = () => {
 
@@ -8,39 +9,83 @@ const FilterMatch = () => {
         filterMatchFucional()
     },[])
 
+
+
   return (
-    <div className='filterMatch'>
-        <div className="filter filter1"> 
-            SHORT BY 
-            <div className="triangleFilter triangleFilter1"></div>
-        </div>
-        <div className="boxFilter boxFilter1">
-        
+    <div className="container_filterMatch">
+        <div className='filterMatch'>
+            <div className="filter filter1"> 
+                SHORT BY 
+                <div className="triangleFilter triangleFilter1"></div>
+            </div>
+            <div className="boxFilter boxFilter1">
+            
+            </div>
+
+            <div className="filter filter2"> 
+                TEAM 
+                <div className="triangleFilter triangleFilter2"></div>
+            </div>
+            <div className="boxFilter boxFilter2">
+            
+            </div>
+
+            <div className="filter filter3"> 
+                STATUS 
+                <div className="triangleFilter triangleFilter3"></div>
+            </div>
+            <div className="boxFilter boxFilter3">
+            
+            </div>
+
+            <div className="filter filter4"> 
+                GAME 
+                <div className="triangleFilter triangleFilter4"></div>
+            </div>
+
+            <div className="boxFilter boxFilter4">
+            
+            </div>
+
         </div>
 
+        <div className="filterMatch_movil">
+            <img className='toggle_filter' src={filterImg} alt="filter logo" />
 
-        <div className="filter filter2"> 
-            TEAM 
-            <div className="triangleFilter triangleFilter2"></div>
-        </div>
-        <div className="boxFilter boxFilter2">
-        
-        </div>
+            <div className="box_filterMatch_movil">
+                <div className=" filter1_movil"> 
+                    SHORT BY 
+                    <div className="triangleFilter triangleFilter1"></div>
+                </div>
+                <div className=" boxFilter1">
+                
+                </div>
 
-        <div className="filter filter3"> 
-            STATUS 
-            <div className="triangleFilter triangleFilter3"></div>
-        </div>
-        <div className="boxFilter boxFilter3">
-        
-        </div>
+                <div className=" filter2_movil"> 
+                    TEAM 
+                    <div className="triangleFilter triangleFilter2"></div>
+                </div>
+                <div className=" boxFilter2">
+                
+                </div>
 
-        <div className="filter filter4"> 
-            GAME 
-            <div className="triangleFilter triangleFilter4"></div>
-        </div>
+                <div className=" filter3_movil"> 
+                    STATUS 
+                    <div className="triangleFilter triangleFilter3"></div>
+                </div>
+                <div className=" boxFilter3">
+                
+                </div>
 
-        <div className="boxFilter boxFilter4">
+                <div className=" filter3_movil"> 
+                    GAME 
+                    <div className="triangleFilter triangleFilter4"></div>
+                </div>
+
+                <div className=" boxFilter4">
+                
+                </div>           
+            </div>
         
         </div>
     </div>
