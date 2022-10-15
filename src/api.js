ae781456866ffd67fbbcd29e8b252be54372b30f
Agustín <agustinngarrone@hotmail.com>
2022-10-14 23:50:35 -0300

    .

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:5000/api/V1`, 
- /*  baseURL: `https://api.cryptocupqatar.io/api/V1`,  */
+  baseURL: `https://api.cryptocupqatar.io/api/V1`, 
   withCredentials:true,
 });
