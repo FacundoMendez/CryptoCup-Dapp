aae3bdaf92828de64178d1ed4e2621056090bfc1
facuMendez <facundo.mendez@live.com.ar>
2022-10-19 13:40:45 -0300

    .

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -40,7 +40,6 @@ const Home = () => {
   return (
     <>
       <div className="containerDapp" >
-        <Preload/>
 
         {connectedPopup ? <PopupError connectedPopup={connectedPopup}  /> : null }
 
