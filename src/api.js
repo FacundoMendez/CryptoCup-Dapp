e064b3c8bcd230b9bba796e2f20b9c4399db6a34
Agustín <agustinngarrone@hotmail.com>
2022-10-19 12:07:07 -0300

    persistencia comentada

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   /* https://cct-api-test.herokuapp.com/api/V1 */ //api pruebas
-  baseURL: `https://api.cryptocupqatar.io/api/V1`, 
+  baseURL: `http://localhost:5000/api/V1`, 
   withCredentials:true,
 });
