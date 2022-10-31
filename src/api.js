1d2c98a5eb3fa0ca213011ec309a123f37caf6f5
Agustín <agustinngarrone@hotmail.com>
2022-10-31 02:07:22 -0300

    añadida fecha a feeds y login google. Actualizado login y registro

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  /* https://cct-test2.herokuapp.com/api/V1 */ //api pruebas
+  /* https://cct-api-test.herokuapp.com/api/V1 */ //api pruebas
   /* https://api.cryptocupqatar.io/api/V1 */ //api real
   
-  baseURL: `http://localhost:5000/api/V1`,
+  baseURL: `https://api.cryptocupqatar.io/api/V1`,
   withCredentials:true,
 });
