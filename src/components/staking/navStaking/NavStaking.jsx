895e0d01536cb2652bb6be429fab60396d85f1c6
FacuMendez <facundo.mendez@live.com.ar>
2022-11-02 00:29:52 -0300

    staking

---
+++ src/components/staking/navStaking/NavStaking.jsx
@@ -0,0 +1,109 @@
+import React, {useState , useEffect, Suspense} from 'react'
+import "./navStaking.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Spinner from '../../config/spinner/Spinner'
+
+
+const NavStaking = ({ladder, setMyStake, setStake , setUnstake }) => {
+
+    const [colorP, setColorP] = useState("#bebdff")
+    const [colorM, setColorM] = useState("white")
+    const [colorR , setColorR] = useState("white")
+
+
+    const colorPred = ( ) => {
+        setColorM('white')
+        setColorP('#bebdff')
+        setColorR('white')
+    }
+    const ColorMyRecord= ( ) => {
+        setColorM('#bebdff')
+        setColorP('white')
+        setColorR('white')
+    }
+    const colorRank = ( ) => {
+        setColorM('white')
+        setColorP('white')
+        setColorR('#bebdff')
+    }
+
+    useEffect(() => {
+        colorPred()
+/*         navLadderActive() */
+    },[])
+
+    return (
+        <Suspense fallback={<Spinner/>}>
+            <div className="nav_staking">
+                <h2>{ladder}</h2>
+                <div className="links_staking">
+                    <ul>
+                        <li className='link_nav_staking' style={{color : colorP}}  onClick={() => {
+                                colorPred()
+                                setStake(true) 
+                                setMyStake(false)
+                                setUnstake(false)  
+                            } }>
+                            STAKE
+                        </li>
+                        <li className='link_nav_staking'  style={{color : colorM}}  onClick={() => {
+                                ColorMyRecord()
+                                setUnstake(true)
+                                setMyStake(false) 
+                                setStake(false) 
+                            }}>
+                            UNSTAKE
+                        </li>
+                        <li className='link_nav_staking'  style={{color : colorR}}  onClick={() =>  {
+                                colorRank()
+                                setStake(false)
+                                setUnstake(false) 
+                                setMyStake(true) 
+                            }}>
+                            MY STAKING
+                        </li>
+                    </ul>
+                </div>
+                <div className="container_links_movile_staking">
+                    <button className="toggle_link_staking" aria-label="Abrir menú">
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+    
+                    <div className="box_links_movile_staking">
+                        <div className="links_movile_staking">
+                            <ul>
+                                <li className='link_nav_staking'   style={{color : colorP}}  onClick={() => {
+                                       setStake(true) 
+                                       setMyStake(false)
+                                       setUnstake(false)   
+                                        colorPred()
+                                    } }> 
+                                    STAKE
+                                </li>
+                                <li className='link_nav_staking'  style={{color : colorM}}  onClick={() => {
+                                        setUnstake(true)
+                                        setMyStake(false) 
+                                        setStake(false) 
+                                        ColorMyRecord()
+                                    } }>
+                                    UNSTAKE
+                                </li>
+                                <li className='link_nav_staking'  style={{color : colorR}}  onClick={() =>  {
+                                        setStake(false)
+                                        setUnstake(false) 
+                                        setMyStake(true) 
+                                        colorRank()
+                                    }}>
+                                    MY STAKE
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Suspense>
+      )
+    }
+    
+export default NavStaking
---
+++ src/components/staking/stake/checkpoints/Checkpoints.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./checkpoints.css"
+
+const Checkpoints = ({setCheckpoints }) => {
+  return (
+    <div className="checkpoints">
+        <div className="box_checkpoints">
+            <div className="card_checkpoint">
+                <h2>Connected With BSC Wallet</h2>
+                <p>If not Connected, Click the "Log In" button in the top right corner</p>
+            </div>
+            <div className="card_checkpoint">
+                <h2>CCT Available to Deposit</h2>
+                <p>0</p>
+            </div>
+            <div className="card_checkpoint">
+                <h2>BNB Available in Wallet</h2>
+                <p>BNB is requiered to pay transaction fees on the BSC Network.</p>
+                <p>0.00</p>
+            </div>
+        </div>
+
+        <label className='label_match_stake'>
+            <input type="radio" name="radio" value="accept_stake" onClick={() => setCheckpoints(true)}/>
+            <span className='span_select_part'> I have read the </span><a href="/">Terms and Conditions.</a>
+        </label>
+
+    </div>
+  )
+}
+
+export default Checkpoints
