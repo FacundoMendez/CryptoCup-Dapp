33464420272fd63276593e5b249b157fcf85b125
ManuelFerreras <manuel.ferreras1@gmail.com>
2022-11-02 09:52:34 -0300

    ranking

--- src/components/ladder/componentsLadders/ranking/Ranking.jsx
+++ src/components/ladder/componentsLadders/ranking/Ranking.jsx
@@ -43,23 +43,23 @@ const Ranking = ({ rankings }) => {
 
   // Render the UI for your table
   return (
-    <table {...getTableProps()} border="1">
-      <thead>
+    <table {...getTableProps()} border="1" className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
-              <th className="table-header" {...column.getHeaderProps()}>{column.render("Header")}</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" {...column.getHeaderProps()}>{column.render("Header")}</th>
             ))}
           </tr>
         ))}
       </thead>
-      <tbody {...getTableBodyProps()}>
+      <tbody {...getTableBodyProps()} className="bg-white divide-y divide-gray-200">
         {rows.map((row, i) => {
           prepareRow(row);
           return (
             <tr className="table-row" {...row.getRowProps()}>
               {row.cells.map((cell) => {
-                return <td className="table-element" {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                return <td className="px-6 py-4 whitespace-nowrap table-element" {...cell.getCellProps()}>{cell.render("Cell")}</td>;
               })}
             </tr>
           );
