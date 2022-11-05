82c0d4983c1d901e9633de6a07d0f21919325897
FacuMendez <facundo.mendez@live.com.ar>
2022-11-04 21:23:41 -0300

    :

--- src/components/ladder/ladderHome/LadderHome.jsx
+++ src/components/ladder/ladderHome/LadderHome.jsx
@@ -120,9 +120,9 @@ const LadderHome = () => {
                         
                             <SplideSlide className='box_splide'>
                                   {Connected.userLogginActive ? 
-                                      <NavLink to="/challengeLadder">  
+                                      /* <NavLink to="/challengeLadder">   */
                                           <img src={challengeImg} alt="Challenge ladder Slider " />
-                                      </NavLink>   
+                                      /* </NavLink>    */
                                 
                                     :
                                       <div onClick={() => setConnectedPopup(true) }>
